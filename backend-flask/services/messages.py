e74cd8c2dbc885b8f857b313e610fcc451ab0ea4
colossus06 <denizkavuk86@gmail.com>
2023-03-18 15:05:52 +0000

    working jwt backend verification

--- backend-flask/services/messages.py
+++ backend-flask/services/messages.py
@@ -13,7 +13,7 @@ class Messages:
         'uuid': '4e81c06a-db0f-4281-b4cc-98208537772a' ,
         'display_name': 'Andrew Brown',
         'handle':  'andrewbrown',
-        'message': 'Cloud is fun!',
+        'message': 'hello from week 5 but 3 actually!',
         'created_at': now.isoformat()
       },
       {
