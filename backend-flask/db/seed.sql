ab7c930d2facbebf82531cd62875c69049519b91
colossus06 <denizkavuk86@gmail.com>
2023-04-01 15:44:48 +0300

    �‍♀️ rework Week 4 SQL RDS

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,12 +1,13 @@
 -- this file was manually created
-INSERT INTO public.users (display_name, email, handle, cognito_user_id)
+INSERT INTO public.users (display_name, handle, cognito_user_id)
 VALUES
-  ('elka', 'topcug@devtechops.dev', 'elka2222222' ,'topcug@devtechops.dev');
+  ('Andrew Brown', 'andrewbrown' ,'MOCK'),
+  ('Andrew Bayko', 'bayko' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'elka2222222' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
