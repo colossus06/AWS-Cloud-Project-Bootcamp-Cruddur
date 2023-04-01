c56a054fe92ee7c0419d8202874b262cfde52e23
colossus06 <denizkavuk86@gmail.com>
2023-04-01 19:08:23 +0300

    🎑 reverification: post-confrm lambda

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,13 +1,13 @@
 -- this file was manually created
-INSERT INTO public.users (display_name, handle, cognito_user_id)
+INSERT INTO public.users (email, display_name, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrewbrown' ,'MOCK'),
-  ('Andrew Bayko', 'bayko' ,'MOCK');
+  -- we can grab the cognito_user_id manually from the Congito Console
+  ('topcug@devtechops.dev', 'simo', 'simo' ,'5dde97c4-f1dc-4507-8ce8-42639d4f8dda');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'simo' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
--- start.sh
+++ start.sh
@@ -1,16 +1,12 @@
 #! /usr/bin/bash
 
-export NO_DB_CONNECTION_URL="postgresql://postgres:ada@localhost:5432"
-
-export CONNECTION_URL="postgresql://postgres:ada@localhost:5432/cruddur"
-
-export PROD_CONNECTION_URL="postgresql://root:huEE33z2Qvl383@cruddur-db-instance.cih7lwdwwpfd.us-east-1.rds.amazonaws.com:5432/cruddur"
-
-export DB_SG_ID="sg-0b4c0bdc8b9f2110d"
-
-export DB_SG_RULE_ID="sgr-08444c3ce5c294a53"
-
-export MY_IP=$(curl icanhazip.com)
+export NO_DB_CONNECTION_URL="postgresql://postgres:ada@localhost:5432"; 
+export CONNECTION_URL="postgresql://postgres:ada@localhost:5432/cruddur"; 
+export PROD_CONNECTION_URL="postgresql://root:huEE33z2Qvl383@cruddur-db-instance.cih7lwdwwpfd.us-east-1.rds.amazonaws.com:5432/cruddur"; 
+export DB_SG_ID="sg-0b4c0bdc8b9f2110d"; 
+export DB_SG_RULE_ID="sgr-08444c3ce5c294a53"; 
+export MY_IP=$(curl icanhazip.com);
+export AWS_DEFAULT_REGION="us-east-1";
 
 echo $NO_DB_CONNECTION_URL
 echo $CONNECTION_URL
@@ -22,3 +18,5 @@ echo $DB_SG_RULE_ID
 # then run -------------  psql $PROD_CONNECTION_URL
 # \l
 # ./db/schema-load--------sql rds vid is finished
+
+# SELECT * FROM users;
